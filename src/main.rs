0e8d159df65153fb1f16ef49acc41b01965c6ade
Literallie <git@l1t.li>
2017-08-06 23:56:52 +0200

    Add GTK message dialog for better Hello World feel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate gtk;
+
+use gtk::prelude::*;
+use gtk::{Window, MessageDialog, DialogFlags, MessageType, ButtonsType};
+
 fn main() {
-    println!("Hello, world!");
+    if gtk::init().is_err() {
+        println!("Well, that didn't last long.");
+        panic!("a minute of silence to our lost brother, gtk");
+    }
+
+    MessageDialog::new(None::<&Window>,
+                       DialogFlags::empty(),
+                       MessageType::Info,
+                       ButtonsType::Ok,
+                       "Hello World").run();
 }
